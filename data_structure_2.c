6452bab816b955fcaa309bf639cf2cf370488752
lizx <1181580150@qq.com>
2021-02-01 21:32:49 +0800

    ww

---
+++ data_structure_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//字符数据类型 char
+//整形     int 
+//短整型   short int
+//长整型 long
+//更长的整型 long long 
+//单精度浮点数 float
+//双精度浮点数 double
+//sizeof（）
+//c语言中有没有字符串？
+
+int main() {
+	printf("");
+}
